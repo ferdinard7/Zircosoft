7e1534b498f17cdd2fa7b3adc79d4880e9f6787c
Ferdinard <tobeferdinard@gmail.com>
2023-08-01 19:33:45 +0100

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./Routes";
-// import Home from "./pages/home/Home";
 
 function App() {
 
