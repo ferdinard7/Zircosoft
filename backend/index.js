c820b95e0bea8187c4dc0c6e86f788c79c582039
omodev <omooladev@gmail.com>
2023-08-02 11:58:42 +0100

    created the main entry file of the backend project and also started the server

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
-
-
-
-
-
+const app = require("./app");
 
 //----------> PORT
-const PORT=process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
+const start = () => {
+  app.listen(PORT, () => {
+    console.log(`Server is listening at PORT ${PORT}`);
+  });
+};
+
+//--------->start server
+start();
