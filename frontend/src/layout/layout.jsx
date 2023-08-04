02abceafe2c79a9b4df7a3b8db1b73e07532afa5
omodev <omooladev@gmail.com>
2023-08-04 10:03:35 +0100

    restructured project

---
+++ frontend/src/layout/layout.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/header/Header";
+import Routes from "../routes/Routes";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Routes />
+    </>
+  );
+};
+
+export default Layout;
